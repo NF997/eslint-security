95061c8c36e2f6243c2f5e8f6896e6ebe3f47688
Nico Fechtner <mail@nicofechtner.de>
2020-08-29 13:20:05 -0400

    Set security/detect-non-literal-require to off

--- default.js
+++ default.js
@@ -206,7 +206,7 @@ module.exports = {
     "security/detect-no-csrf-before-method-override": "error",
     "security/detect-non-literal-fs-filename": "error",
     "security/detect-non-literal-regexp": "error",
-    "security/detect-non-literal-require": "warn",
+    "security/detect-non-literal-require": "off", // To avoid duplicates.
     "security/detect-object-injection": "error",
     "security/detect-possible-timing-attacks": "warn",
     "security/detect-pseudoRandomBytes": "error"
